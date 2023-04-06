57c8a112925d9540917320ad63fb63bc46daaa3c
RareBowl2 <129699517+RareBowl2@users.noreply.github.com>
2023-04-05 20:58:30 -0600

    Add files via upload

--- js/ElonMuskSim.js
+++ js/ElonMuskSim.js
@@ -1,7 +1,7 @@
 
+
 window.onload = function () {
     // TODO:: Do your initialization job
-
     // add eventListener for tizenhwkey
     document.addEventListener('tizenhwkey', function(e) {
         if(e.keyName == "back")
